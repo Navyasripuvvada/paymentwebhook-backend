07c95babba254b9c791b7da78134f8e37dc5f54c
Navyasripuvvada <puvvadanavyasri5809@gmail.com>
2026-04-21 10:27:26 +0530

    added cors origin

--- src/app.ts
+++ src/app.ts
@@ -3,7 +3,7 @@ import cors from "cors";
 import paymentroutes from "./routes/paymentroutes"
 const app=express();
 app.use(express.json());
-app.use(cors());
+app.use(cors({origin:"https://webhooh-frontend.netlify.app/"}));
 app.get("/", (req, res) => {
   res.send("Backend is running");
 });
